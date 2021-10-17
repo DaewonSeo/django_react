2cc3c5ce890c730023cf12031564a083a9ea6688
DaewonSeo <saz0549@naver.com>
2021-10-17 19:12:48 +0900

    View단APIView, generic, 데코레이터 등 다양한 방식으로 구현

--- instagram/serializers.py
+++ instagram/serializers.py
@@ -1,8 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.db import models
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Post
 
-class PostSerializer(ModelSerializer):
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['email', 'username']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author_username = serializers.ReadOnlyField(source='author.username')
+    # author = AuthorSerializer()
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = [
+            'pk',
+            'author_username',
+            'message',
+            'created_at',
+            'is_public',
+        ]
+
--- instagram/views.py
+++ instagram/views.py
@@ -1,8 +1,31 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from typing_extensions import ParamSpecArgs
 from django.shortcuts import render
+from rest_framework import generics
 from rest_framework.viewsets import ModelViewSet
 from .serializers import PostSerializer
 from .models import Post
+
+#genrics를 상속 받아서 구현하는 방법
+# class PublicPostListAPIView(generics.ListAPIView):
+#     queryset = Post.objects.filter(is_public=True)
+#     serializer_class = PostSerializer
+
+# APIView로 직접 구현하는 방법
+# class PublicPostListAPIView(APIView):
+#     def get(self, request):
+#         qs = Post.objects.filter(is_public=True)
+#         serializer = PostSerializer(qs, many=True)
+#         return Response(serializer.data)
+
+#장식자를 활용하여 함수로 직접 구현하는 방법
+@api_view(['GET'])
+def public_post_list(request):
+    qs = Post.objects.filter(is_public=True)
+    serializer = PostSerializer(qs, many=True)
+    return Response(serializer.data)
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
